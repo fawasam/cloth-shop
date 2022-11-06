69638a2a7239fcc1d129f976763f1ed7dee4e694
fawasam <fawasam5@gmail.com>
2022-11-06 10:08:27 +0530

    created Header component and linked to pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage";
 import Shop from "./pages/shop/Shop";
+import Header from "./components/header/header";
 
 const TopicsList = () => (
   <div>
@@ -21,6 +22,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route path="/shop" element={<Shop />} />
