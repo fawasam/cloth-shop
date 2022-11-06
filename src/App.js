fa08eab3c677f599a76db8ee9554cc8413b97fc3
fawasam <fawasam5@gmail.com>
2022-11-06 09:54:58 +0530

    updating app with shop page, collection preview

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import Homepage from "./pages/homepage/homepage";
 import "./App.css";
-
 import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import Homepage from "./pages/homepage/homepage";
+import Shop from "./pages/shop/Shop";
 
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Hats page</h1>
-    </div>
-  );
-};
 const TopicsList = () => (
   <div>
     <h1>TOPIC LIST PAGE</h1>
@@ -30,7 +23,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          <Route path="/hats" element={<HatsPage />} />
+          <Route path="/shop" element={<Shop />} />
+
           <Route path="/topics" element={<TopicsList />} />
           <Route path="/topics/:topicId" element={<TopicsDetail />} />
         </Routes>
